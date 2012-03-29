4fa0886c2319386a4cbadf3a2a835cb13211c567
parlu048 <parlu048@student.liu.se>
2012-03-29 14:17:37 +0200

    Har lagt in Mikas kod i styrenheten

--- avr/styr_module/main_styrenhet.c
+++ avr/styr_module/main_styrenhet.c
@@ -3,65 +3,74 @@
 #include <avr/io.h>
 #include <inttypes.h>
 
-<<<<<<< HEAD
-
-//Constants
-#define CROSSING 0x00 //onödig? väntläge tom mitten?
-#define CROSSING_LEFT 0x01
-#define CROSSING_RIGHT 0x02
-#define CROSSING_FORWARD 0x03
-
-#define STRAIGHT 0x04
-
-#define TURN 0x05 // onödig?
-#define TURN_LEFT 0x06
-#define TURN_RIGHT 0x07
-#define TURN_FORWARD 0x08
-
-#define MANUAL 0x09 // onödig?
-#define MANUAL_LEFT 0x0A
-#define MANUAL_RIGHT 0x0B
-#define MANUAL_FORWARD 0x0C
-#define MANUAL_BACK 0x0D
-#define MANUAL_ROTATE_LEFT 0x0E
-#define MANUAL_ROTATE_RIGHT 0x0F
-
-#define ROTATION_COMPLETE 0x10 //onödig?
-=======
 #include "../TWI/TWI.h"
 #include "motor.h"
 #include "../commands.h"
->>>>>>> fa147aff99211a71087ec5c53ac62d27095dcbcb
 
+//Initialize interrupts
+void interrupts(void){
+	sei();
+	MCUCR |= (MCUCR & 0xfc) | ((0<<ISC00) | (1<<ISC01));
+    GICR |= (1<<INT0);
+}
+
+//Interrupt routine
+ISR(INT0_vect){
+	x = 1-x;
+	return;
+}
 
 //MAIN
 int main(void)
 {
-<<<<<<< HEAD
 
 setup_motor();
 
 uint8_t TWI_read(uint8_t* s);
 void TWI_init(uint8_t sla);
+setup_motor();
+TWI_init(CONTROL_ADDRESS);
+sei();
+interrupts();
+uint8_t s[10];
 
 // Loop
 while (1){
-=======
 
-	setup_motor();
-	TWI_init(CONTROL_ADDRESS);
-	sei();
 
-	uint8_t s[10];
->>>>>>> fa147aff99211a71087ec5c53ac62d27095dcbcb
+	uint8_t len = TWI_read(s);
 
-	// Loop
-	while (1){
+if(len){
 
-		uint8_t len = TWI_read(s);
-		if(len){
+	if(x == 0){
+
+		switch(s[2]){
+			case LEFT:
+			manual_left();
+			break;
+			case RIGHT:
+			manual_right();
+			break;
+			case FORWARD:
+			manual_forward();
+			break;
+			case REVERSE:
+			manual_reverse();
+			break;
+			case ROTATE_LEFT:
+			rotate_left();
+			break;
+			case ROTATE_RIGHT:
+			rotate_right();
+			break;
+			case STOP:
+			manual_stop();
+			break;
+			}
+		}
+		else{
 			switch(s[0]){
-			/*case CROSSING:
+			case CROSSING:
 				switch(s[2]){
 				case CROSSING_LEFT:
 					rotate_left();
@@ -89,82 +98,10 @@ while (1){
 					turn_forward();
 					break;
 				}
-				break;*/
-			case CMD_MANUAL:
-				switch(s[2]){
-				case LEFT:
-					manual_left();
-					break;
-				case RIGHT:
-					manual_right();
-					break;
-				case FORWARD:
-					manual_forward();
-					break;
-				case REVERSE:
-					manual_reverse();
-					break;
-				case ROTATE_LEFT:
-					rotate_left();
-					break;
-				case ROTATE_RIGHT:
-					rotate_right();
-					break;
-				case STOP:
-					manual_stop();
-					break;
-				}
 				break;
 			}
-		}
-<<<<<<< HEAD
-	case STRAIGHT:
-		run_straight(difference,direction);
-	case TURN:
-		switch(s[2]){
-		case TURN_LEFT:
-			turn_left();
-			break;
-		case TURN_RIGHT:
-			turn_right();
-			break;
-		case TURN_FORWARD:
-			turn_forward();
-			break;
-		}
-	case MANUAL
-		switch(s[2]){
-		case MANUAL_LEFT:
-			manual_left();
-			break;
-		case MANUAL_RIGHT:
-			manual_right();
-			break;
-		case MANUAL_FORWARD:
-			manual_forward();
-			break;
-		case MANUAL_BACK:
-			manual_back();
-			break;
-		case MANUAL_ROTATE_LEFT:
-			rotate_left();
-			break;
-		case MANUAL_ROTATE_RIGHT:
-			rotate_right();
-			break;
+			}
 		}
 	}
-	
-	}
-		
 }
-//	while(1){
-//		asm volatile("nop");
-//		}	
-	}	
-=======
-	}
-		
 }
-	
->>>>>>> fa147aff99211a71087ec5c53ac62d27095dcbcb

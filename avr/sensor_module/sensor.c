4fb1ce9aca6af342abcb8e94e104b3121a755a96
parlu048 <parlu048@student.liu.se>
2012-04-16 15:42:47 +0200

    Vaggsensor inlagt i sensor.c

--- avr/sensor_module/sensor.c
+++ avr/sensor_module/sensor.c
@@ -1,6 +1,7 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <inttypes.h>
+#include<avr/pgmspace.h>
 
 #include "../TWI/TWI.h"
 #include "../utility/send.h"
@@ -9,6 +10,8 @@
 
 #define GYRO_TURN_LEFT -0x7fffffff
 #define GYRO_TURN_RIGHT 0x7fffffff //Tolkas de decimalt??
+
+#define DIFFERENCE 0x00;
 
 
 
@@ -23,16 +26,76 @@ volatile uint8_t gyro_initialize = 0;
 volatile uint8_t global_tape = 0;
 volatile uint8_t tape_counter = 0;
 volatile uint8_t timer = 0;
+
+volatile int long_ir_1_value;//Värdet på den analoga spänning som lång avståndsmätare 1 gett(pinne 34/PA6)
+volatile int long_ir_2_value;//Värdet på den analoga spänning som lång avståndsmätare 2 gett(pinne 38/PA2)
+volatile int short_ir_1_value;//Värdet på den analoga spänning som kort avståndsmätare 1 gett(pinne 37/PA3)
+volatile int short_ir_2_value;//Värdet på den analoga spänning som kort avståndsmätare 2 gett(pinne 36/PA4)
+volatile int short_ir_3_value;//Värdet på den analoga spänning som kort avståndsmätare 3 gett(pinne 35/PA5)
+volatile int itr_long_ir_1 = 0;
+volatile int itr_long_ir_2 = 0;
+volatile int itr_short_ir_1 = 0;
+volatile int itr_short_ir_2 = 0;
+volatile int itr_short_ir_3 = 0;
 volatile uint8_t long_ir_left_values[4];
 volatile uint8_t long_ir_right_values[4];
 volatile uint8_t short_ir_left_values[4];
 volatile uint8_t short_ir_right_values[4];
 volatile uint8_t short_ir_back_values[4];
-volatile uint8_t itr_long_ir_left = 0;
-volatile uint8_t itr_long_ir_right = 0;
-volatile uint8_t itr_short_ir_left = 0;
-volatile uint8_t itr_short_ir_right = 0;
-volatile uint8_t itr_short_ir_back = 0;
+
+//Referensevärden
+volatile uint8_t voltage_ref_short1[120	119	118	117	116	115	114	113	112	111	110	109	108	
+									107	106	105	104	103	102	101	100	99	98	97	96	95	
+									94	93	92	91	90	89	88	87	86	85	84	83	82	
+									81	80	79	78	77	76	75	74	73	72	71	70	69	
+									68	67	66	65	64	63	62	61	60	59	58	57	56	
+									55	54	53	52	51	50	49	48	47	46	45	44	43	
+									42	41	40	39	38	37	36	35	34	33	32	31	30];
+
+volatile uint8_t distance_ref_short1[0  0	0   1   1   1   2   2	2   3   3	3   4 
+									 4	5	5   6   6   7   7   8   8   9   9   10  10    
+									 11	11  12  12	13  13  14  14  15	15  16  16  17  
+									 18 19  20  21  22  23  24  25  26  27  28  29  30 
+									 31 32  33  34  36  38  40  41  42  44  45  47  48
+									 49 51  53	55  57	59	61	62	64  66	69	71	74
+									 77	80	83	86	90	93	96	100	104	108	112	119	127];
+
+volatile uint8_t voltage_ref_short2[120	119	118	117	116	115	114	113	112	111	110	109	108	
+									107	106	105	104	103	102	101	100	99	98	97	96	95	
+									94	93	92	91	90	89	88	87	86	85	84	83	82	
+									81	80	79	78	77	76	75	74	73	72	71	70	69	
+									68	67	66	65	64	63	62	61	60	59	58	57	56	
+									55	54	53	52	51	50	49	48	47	46	45	44	43	
+									42	41	40	39	38	37	36	35	34	33	32	31	30];
+
+volatile uint8_t distance_ref_short2[0  0	0   1   1   1   2   2	2   3   3	3   4 
+									 4	5	5   6   6   7   7   8   8   9   9   10  10    
+									 11	11  12  12	13  13  14  14  15	15  16  16  17  
+									 18 19  20  21  22  23  24  25  26  27  28  29  30 
+									 31 32  33  34  36  38  40  41  42  44  45  47  48
+									 49 51  53	55  57	59	61	62	64  66	69	71	74
+									 77	80	83	86	90	93	96	100	104	108	112	119	127];
+
+volatile uint8_t voltage_ref_short3[120	119	118	117	116	115	114	113	112	111	110	109	108	
+									107	106	105	104	103	102	101	100	99	98	97	96	95	
+									94	93	92	91	90	89	88	87	86	85	84	83	82	
+									81	80	79	78	77	76	75	74	73	72	71	70	69	
+									68	67	66	65	64	63	62	61	60	59	58	57	56	
+									55	54	53	52	51	50	49	48	47	46	45	44	43	
+									42	41	40	39	38	37	36	35	34	33	32	31	30];
+
+volatile uint8_t distance_ref_short3[0  0	0   1   1   1   2   2	2   3   3	3   4 
+									 4	5	5   6   6   7   7   8   8   9   9   10  10    
+									 11	11  12  12	13  13  14  14  15	15  16  16  17  
+									 18 19  20  21  22  23  24  25  26  27  28  29  30 
+									 31 32  33  34  36  38  40  41  42  44  45  47  48
+									 49 51  53	55  57	59	61	62	64  66	69	71	74
+									 77	80	83	86	90	93	96	100	104	108	112	119	127];
+
+volatile uint8_t voltage_ref_long1[]; // Behöver endast en bransch!
+volatile uint8_t voltage_ref_long2[]; // Behöver endast en bransch!
+
+
 
 uint8_t lowest_value(uint8_t *list);
 
@@ -63,33 +126,34 @@ ISR(ADC_vect){
 		switch(i){
 		case 2:
 			// Spara värde från ad-omvandligen.
-			long_ir_right_values[itr_long_ir_right]= ADCH;
+			long_ir_2_values[itr_long_ir_2]= ADCH;
 			// Räkna upp iteratorn.
-      		if(++itr_long_ir_right > 3) itr_long_ir_right = 0;
+      		if(++itr_long_ir_2 > 3) itr_long_ir_2 = 0;
 			break;
 		case 3:
 			// Spara värde från ad-omvandligen.
-			short_ir_left_values[itr_short_ir_left]= ADCH;
+			short_ir_1_values[itr_short_ir_l]= ADCH;
 			// Räkna upp iteratorn.
-			if(++itr_short_ir_left > 3) itr_short_ir_left = 0;
+			if(++itr_short_ir_1 > 3) itr_short_ir_1 = 0;
 			break;
 		case 4:
 			// Spara värde från ad-omvandligen.
-			short_ir_right_values[itr_short_ir_right]= ADCH;
+			short_ir_2_values[itr_short_ir_2]= ADCH;
 			// Räkna upp iteratorn.
-			if(++itr_short_ir_right > 3) itr_short_ir_right = 0;
+			if(++itr_short_ir_2 > 3) itr_short_ir_2 = 0;
+			TWI_write(DIFFERENCE, 3, difference());
 			break;
 		case 5:
 			// Spara värde från ad-omvandligen.
-			short_ir_back_values[itr_short_ir_back]= ADCH;
+			short_ir_3_values[itr_short_ir_3]= ADCH;
 			// Räkna upp iteratorn.
-			if(++itr_short_ir_back > 3) itr_short_ir_back = 0;
+			if(++itr_short_ir_3 > 3) itr_short_ir_3 = 0;
 			break;
 		case 6:
 			// Spara värde från ad-omvandligen.
-			long_ir_left_values[itr_long_ir_left]= ADCH;
+			long_ir_2_values[itr_long_ir_2]= ADCH;
 			// Räkna upp iteratorn.
-			if(++itr_long_ir_left > 3) itr_long_ir_left = 0;
+			if(++itr_long_ir_2 > 3) itr_long_ir_2 = 0;
 			break;
 		case 7: 
 			tape_value = ADCH;
@@ -158,15 +222,31 @@ uint8_t min(uint8_t value_one, uint8_t value_two){
 		return value_two;
 }
 
+
 //Subrutin som plockar ut det minsta värdet i arrayen
-uint8_t lowest_value(uint8_t list[])
+uint8_t lowest_value(uint8_t *list)
 {
-	uint8_t minimum = list[0];
-	for(uint8_t itr = 1; itr < 4; ++itr){
-		if(minimum>list[itr])
-			minimum=list[itr];
+	int minimum = list[0];
+	int itr;
+	for(itr=1;itr<4;itr++){
+		if(minimum>list[itr]){
+		minimum=list[itr];
+		}
 	}
 	return minimum;
+}
+
+//Differensfunktion
+uint8_t difference(){
+
+uint8_t diff;
+
+uint8_t short1 = lowest_value(short_ir_1_values),
+uint8_t short2 = lowest_value(short_ir_2_values);
+
+diff = 127 + short1 - short2;
+
+return diff;
 }
 
 //Subrutin för tejpdetektering

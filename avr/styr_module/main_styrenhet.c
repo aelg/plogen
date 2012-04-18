59ed30ef63da85217222fbe31b527dd4fd8483e4
parlu048 <parlu048@student.liu.se>
2012-04-18 11:04:03 +0200

    motorfix

--- avr/styr_module/main_styrenhet.c
+++ avr/styr_module/main_styrenhet.c
@@ -122,12 +122,71 @@ int main(void)
 
 		if(len){
 			if(x == 0){
-				manual_control(s);
+		/*		switch(s[2]){
+		case LEFT:
+			manual_left();
+			break;
+		case RIGHT:
+			manual_right();
+			break;
+		case FORWARD:
+			manual_forward();
+			break;
+		case REVERSE:
+			manual_reverse();
+			break;
+		case ROTATE_LEFT:
+			rotate_left();
+			break;
+		case ROTATE_RIGHT:
+			rotate_right();
+			break;
+		case STOP:
+			manual_stop();
+			break;
+		
+	}
+	*/
+			//	manual_control(s);
 			}	
 			else{ 
-				auto_control(s);
+
+			switch(s[0]){
+			case STRAIGHT:
+				run_straight(s[2]);
+				break;
+			case CROSSING:
+				switch(s[2]){
+				case CROSSING_LEFT:
+					rotate_left();
+					break;
+				case CROSSING_RIGHT:
+					rotate_right();
+					break;
+				case CROSSING_FORWARD:
+					drive_forward();
+					break;
+				}
+				break;
+			case TURN:
+				switch(s[2]){
+				case TURN_LEFT:
+					turn_left();
+					break;
+				case TURN_RIGHT:
+					turn_right();
+					break;
+				case TURN_FORWARD:
+					turn_forward();
+					break;
+				}
+				break;
 			}
+
+				//auto_control(s);
+			}	
 		}
+		
 	}
 }
 	
